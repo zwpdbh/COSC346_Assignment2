8a2f9bcfdd66c017013b709ba9f8586e2cc97547
zwpdbh <hyperion_z@outlook.com>
2016-09-27 21:41:04 +1300

    < lab 22

--- MyPDFViewer/MyPDFViewer/AppDelegate.swift
+++ MyPDFViewer/MyPDFViewer/AppDelegate.swift
@@ -14,61 +14,59 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     
     // MARK: - pdf variable
-    var currentPDFPage: PDFPage?
-    var currentPageIndex: Int = 1 {
-        didSet {
-            currentPageDisplay.stringValue = "\(currentPageIndex)/\(self.totalPageCount)"
-        }
-    };
     
-    var totalPageCount = 0;
-    var pdfDoc: PDFDocument? {
-        didSet {
-            updateWithDocInfo()
-        }
-    }
-    
-    
+    var pdfSet: PDFSet?
+    var pdfDoc: PDFDoc?
+
     // MARK: - outlet and action
+    
+    // open file action
     @IBAction func openFile(sender: NSMenuItem) {
         let panel = NSOpenPanel()
+        panel.allowsMultipleSelection = true;
+        
         panel.beginWithCompletionHandler { (result) in
             if(result == NSFileHandlingPanelOKButton) {
-                var docURL: NSURL?
-                docURL = panel.URLs[0]
-                self.pdfDoc = PDFDocument(URL: docURL)
-                self.pdfView.setDocument(self.pdfDoc)
+                self.pdfSet = PDFSet(urlSet: panel.URLs)
+                self.pdfView.setDocument(self.pdfSet?.getCurrentPDFDoc().getPDFDocument())
             }
         }
     }
     
     // go to previous page
     @IBAction func previousPage(sender: NSButton) {
-       pdfView.goToPreviousPage(sender)
-        if currentPageIndex > 1 {
-            currentPageIndex -= 1
-            print("currentPage go to: \(currentPageIndex)")
-        }
+        self.pdfDoc?.moveToPreviouPage()
+        pdfView.goToPreviousPage(sender)
+        
+        updateView()
     }
     
     
     // go to the next page
     @IBAction func nextPage(sender: NSButton) {
+        self.pdfDoc?.moveToNextPage()
         pdfView.goToNextPage(sender)
-        if currentPageIndex < self.totalPageCount {
-            currentPageIndex += 1
-            print("currentPage go to: \(currentPageIndex)")
-        }
+        updateView()
     }
     
+    // go to the Given page
     @IBAction func goToGivenPage(sender: NSTextField) {
         if let pageNumber = Int(sender.stringValue) {
-            let page = self.pdfDoc?.pageAtIndex(pageNumber - 1)
-            self.pdfView.goToPage(page)
-            self.currentPageIndex = pageNumber
+            self.pdfDoc?.moveToPageAt(pageNumber)
+            updateView()
         }
     }
     
+    
+    // if there is not one pdf, then click this can go to previous one
+    @IBAction func goToPreviousPDF(sender: NSButton) {
+
+    }
+    
+    // if there is not one pdf, then click this can to to next one
+    @IBAction func goToNextPDF(sender: NSButton) {
+
+    }
     
     @IBOutlet weak var currentPageDisplay: NSTextField!
     
@@ -76,12 +74,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     @IBOutlet weak var window: NSWindow!
 
-    func updateWithDocInfo() {
-        if let doc = self.pdfDoc {
-            currentPDFPage = pdfView.currentPage()
-            totalPageCount = doc.pageCount()
-            currentPageIndex = 1
-        }
+    func updateView() {
+        self.currentPageDisplay.stringValue = "\(self.pdfDoc?.getCurrentPage())/\(self.pdfDoc?.getTotalPages()))"
     }
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
---
+++ MyPDFViewer/MyPDFViewer/PDFData.swift
@@ -0,0 +1,105 @@
+//
+//  PDFData.swift
+//  MyPDFViewer
+//
+//  Created by zwpdbh on 9/27/16.
+//  Copyright © 2016 Otago. All rights reserved.
+//
+
+import Cocoa
+import Quartz
+
+class PDFSet {
+    private var pdfDocSet: Array<PDFDoc>
+    
+    private var fileIndex: Int
+    
+    init(urlSet: Array<NSURL>) {
+        var dataSet = Array<PDFDoc>()
+        
+        for i in 0..<urlSet.count {
+            dataSet.append(PDFDoc(pdfURL: urlSet[i]))
+        }
+        self.pdfDocSet = dataSet
+        
+        self.fileIndex = 0
+    }
+    
+    internal func getPDFAtIndex(index: Int) -> PDFDoc? {
+        if index >= 0 && index < self.pdfDocSet.count {
+            self.fileIndex = index
+            return self.pdfDocSet[index]
+        } else {
+            return nil
+        }
+    }
+    
+    // MARK: - PDFDoc operation
+    internal func getPreviousPDFDoc() -> PDFDoc{
+        fileIndex = fileIndex - 1 < 0 ? (pdfDocSet.count - 1) : (fileIndex - 1)
+        return self.pdfDocSet[fileIndex]
+    }
+    
+    internal func getNextPDFDoc() -> PDFDoc {
+        self.fileIndex = (fileIndex + 1) % self.pdfDocSet.count
+        return self.pdfDocSet[fileIndex]
+    }
+    
+    internal func getCurrentPDFDoc() -> PDFDoc {
+        return self.pdfDocSet[fileIndex]
+    }
+}
+
+
+
+class PDFDoc {
+    private var pdfURL: NSURL
+    private var pdfDoc: PDFDocument
+    private var title: String?
+    private var totalPages: Int
+    private var currentPDFPage: PDFPage
+    private var currentPage: Int {
+        didSet {
+            self.currentPDFPage = self.pdfDoc.pageAtIndex(currentPage)
+        }
+    }
+    
+    init(pdfURL: NSURL) {
+        self.pdfURL = pdfURL
+        self.pdfDoc = PDFDocument(URL: pdfURL)
+        self.totalPages = self.pdfDoc.pageCount()
+        self.currentPage = 1
+        self.currentPDFPage = self.pdfDoc.pageAtIndex(currentPage)
+    }
+    
+    internal func getCurrentPage() -> Int {
+        return self.currentPage
+    }
+    
+    internal func moveToNextPage() -> PDFPage {
+        self.currentPage = (self.currentPage + 1) >= self.totalPages ? (self.totalPages - 1) : (self.currentPage + 1)
+        return self.currentPDFPage
+    }
+    
+    internal func moveToPreviouPage() -> PDFPage {
+        self.currentPage = (self.currentPage - 1) <= 0 ? 0 : (self.currentPage - 1)
+        return self.currentPDFPage
+    }
+    
+    internal func moveToPageAt(page: Int) -> PDFPage? {
+        if page >= 1 && page <= self.totalPages {
+            self.currentPage = page
+            return self.currentPDFPage
+        } else {
+            return nil
+        }
+    }
+    
+    internal func getTotalPages() -> Int {
+        return self.totalPages
+    }
+    
+    internal func getPDFDocument() ->PDFDocument {
+        return self.pdfDoc
+    }
+}
