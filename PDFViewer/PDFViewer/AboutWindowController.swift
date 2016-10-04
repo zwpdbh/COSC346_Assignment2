b3bce17642a71d26b59ed8e1c0f8f7c12e2a5337
zwpdbh <hyperion_z@outlook.com>
2016-10-04 22:21:17 +1300

    < about window can be considered finished

--- PDFViewer/PDFViewer/AboutWindowController.swift
+++ PDFViewer/PDFViewer/AboutWindowController.swift
@@ -10,10 +10,13 @@ import Cocoa
 
 class AboutWindowController: NSWindowController {
 
+    @IBOutlet weak var aboutImageView: NSImageView!
+    
     override func windowDidLoad() {
         super.windowDidLoad()
 
         // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+        aboutImageView.image = NSImage(named: "pages-icon")
     }
     
     override var windowNibName: String? {
