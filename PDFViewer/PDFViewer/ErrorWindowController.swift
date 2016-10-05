922b36558a65864694e2f7f57aa98bae7cb23856
zwpdbh <hyperion_z@outlook.com>
2016-10-05 13:52:46 +1300

    << use tableView to load error message when can not find associated pdf with note

--- PDFViewer/PDFViewer/ErrorWindowController.swift
+++ PDFViewer/PDFViewer/ErrorWindowController.swift
@@ -8,17 +8,21 @@
 
 import Cocoa
 
-class ErrorWindowController: NSWindowController {
+class ErrorWindowController: NSWindowController, NSTableViewDataSource {
 
+    @IBOutlet weak var errorMessageTableView: NSTableView!
     
-    @IBOutlet weak var errorTableView: NSScrollView!
-    
-    var errorURLs = Array<NSURL>()
+    var errorURLs = Array<NSURL>() {
+        didSet {
+            self.errorMessageTableView.reloadData()
+        }
+    }
     
     override func windowDidLoad() {
         super.windowDidLoad()
 
         // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+        self.errorMessageTableView.setDataSource(self)
     }
     
     override var windowNibName: String? {
@@ -28,5 +32,18 @@ class ErrorWindowController: NSWindowController {
     func updateError(invalidURLs urls: Array<NSURL>) {
         self.errorURLs = urls
     }
+ 
     
+    func numberOfRowsInTableView(tableView: NSTableView) -> Int {
+        return self.errorURLs.count
+    }
+    
+    func tableView(tableView: NSTableView, objectValueForTableColumn tableColumn: NSTableColumn?, row: Int) -> AnyObject? {
+        
+        if tableColumn?.title == "PDF Name" {
+            return self.errorURLs[row].lastPathComponent
+        } else {
+            return self.errorURLs[row].absoluteString
+        }
+    }
 }
